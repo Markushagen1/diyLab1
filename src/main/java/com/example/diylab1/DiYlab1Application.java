03584975d605719fe7aebb326f0e1c2f6f307627
Markushagen1 <57091267+Markushagen1@users.noreply.github.com>
2024-09-13 09:31:25 +0200

    first commit

---
+++ src/main/java/com/example/diylab1/DiYlab1Application.java
@@ -0,0 +1,13 @@
+package com.example.diylab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiYlab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiYlab1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/diylab1/DiYlab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diylab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiYlab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
